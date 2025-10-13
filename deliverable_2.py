13bbca8c0d0267a0469b246a82c39b3863e21ecb
Carlynmatar <carlynmatar@gmail.com>
2025-10-12 20:35:52 -0400

    Update deliverable_2.py
    (Posting another commit, with more comments, since I forgot to add a description to last one).
    
    I turned the data into arrays, and filtered it to only keep the study hours above 2 hours, and its respective exam score. In order to do that, I used condition and "for loops" to go through the arrays to find the positions where the study hours were larger than 2.

--- deliverable_2.py
+++ deliverable_2.py
@@ -1,8 +1,10 @@
 
-
+# Importing the data 
 import pandas as pd
 data = pd.read_csv('student_exam_scores.csv')
 
+
+# Turning data from collums into arrays
 import numpy as np
 
 study_hours = np.array(data["hours_studied"])
@@ -16,14 +18,15 @@ for i in range(len(study_hours)):
     if study_hours[i] > 2:
         positions = positions + [i]
         
-# New arrays
+# Using the positions to filter the data
 filtered_study_hours = []
 filtered_exam_scores = []
 
 for i in positions:
-    filtered_study_hours = filtered_study_hours + [study_hours[i]]
-    filtered_exam_scores = filtered_exam_scores + [exam_scores[i]]
+    filtered_study_hours += [study_hours[i]]
+    filtered_exam_scores += [exam_scores[i]]
 
+# Turning the data back into arrays and printing them
 filtered_study_hours = np.array(filtered_study_hours)
 filtered_exam_scores = np.array(filtered_exam_scores)
 
