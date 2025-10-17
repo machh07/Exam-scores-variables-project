280a82c54e08d7ab8dd1749713de2b81e5bdcff6
Neshama g <gozlanneshama@gmail.com>
2025-10-17 13:13:10 -0400

    Adding new plots

--- deliverable_2.py
+++ deliverable_2.py
@@ -1,14 +1,17 @@
-
 # Importing the data 
 import pandas as pd
-scores = pd.read_csv('student_exam_scores.csv')
+import numpy as np
+
+
+
+data = pd.read_csv('student_exam_scores.csv')
 
 
 # Turning data from collums into arrays
-import numpy as np
 
-study_hours = np.array(scores["hours_studied"])
-exam_scores = np.array(scores["exam_score"])
+
+study_hours = np.array(data["hours_studied"])
+exam_scores = np.array(data["exam_score"])
 
 
 # Positions where study hours > 2
@@ -33,61 +36,32 @@ filtered_exam_scores = np.array(filtered_exam_scores)
 print("Filtered Study Hours:", filtered_study_hours)
 print("Filtered Exam Scores:", filtered_exam_scores)
 
-#Create the plot
-import matplotlib.pyplot as plt
-x = filtered_study_hours
-y = filtered_exam_scores
 
-#Create a scatter plot
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+
+bins = [2, 4, 6, 8 ,10 ,12] 
+labels = ["2-4", "4-6", "6-8","8-10","10-12"]
+
+x = np.arange(len(filtered_exam_scores))  
+y = filtered_exam_scores                 
+
+plt.bar(x, y, color='pink')            
+plt.xlabel("Hours Studied")    
+plt.ylabel("Exam Scores")
+plt.title("Exam Scores for Students Who Studied > 2 Hours")
+                       
+plt.show()
+
+# plot of any type containing 2 subplots side by side (counts as 1)
 plt.scatter(x,y, color="red", alpha = 0.7)
 plt.title("Scatter Plot of Exam Scores vs Study Time")
 plt.xlabel("Number of hours studied")
 plt.ylabel("Exam scores")
+plt.subplot(1,2,2)
+plt.scatter(x,y)
 plt.show()
-
-#Create a histogram
-plt.hist(y, bins=25, color='purple', alpha=0.8)
-plt.xlabel("Exam scores")
-plt.ylabel("Frequency")
-plt.title("Histogram of Exam Scores")
-plt.show()
-
-#Create a bar graph
-avg_scores = []
-
-ranges = [(2, 4), (4, 6), (6, 8), (8, 10), (10, 12)]
-labels = ['2-4', '4-6', '6-8', '8-10', '10-12']
-
-plt.bar(labels, avg_scores, color='orange')
-plt.title('Average Exam Scores by Study Hour Ranges')
-plt.xlabel('Study Hours')
-plt.ylabel('Average Exam Score')
-plt.show()
-
-
-#Create pie chart
-plt.pie(avg_scores,
-        labels=labels,
-        autopct='%1.1f%%',    
-        startangle=140,         
-        colors=['orange', 'blue', 'cyan', 'yellow', 'green'])
-
-plt.title('Average Exam Scores by Study Hour Ranges')
-plt.show()
-
-#Import seaborn
-import seaborn as sns
-sns.set_theme()
-
-#Create plot of any type using grid
-lineplot=sns.relplot(
-    data=scores, 
-    x="hours_studied",
-    y="exam_score", 
-    kind="line",
-    errorbar="sd")
-#Add a title to line plot
-lineplot.fig.suptitle("Exam Score vs Hours Studied")
-#You should comment your code with a quick explanation about each plot !!!
 
 
